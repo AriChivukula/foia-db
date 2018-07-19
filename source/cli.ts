060c2962e4b018e5ca9c359b932589bf23ea3c7c
Ari Chivukula <ari@chivuku.la>
2018-07-19 15:49:39 -0700

    Part 3
    #2

--- source/cli.ts
+++ source/cli.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import { existsSync, readdirSync, readFileSync, writeFileSync } from "fs";
 import * as yargs from "yargs";
 
 if (require.main === module) {
@@ -34,5 +35,31 @@ function validateConfig(
   lint: boolean,
   release: boolean,
 ): void {
+  const config: string = JSON.parse(readFileSync(".foia-db"));
+  Object.keys(config).forEach((table) => {
+    validateTable(lint, release, table);
+  });
+}
 
+function validateTable(
+  lint: boolean,
+  release: boolean,
+  table: object,
+): void {
+  if (!existsSync("data/" + table.name)) {
+    throw new Error("Missing data for " + table.name);
+  }
+  readdirSync("data/" + table.name).forEach((document_path) => {
+    validateDocument(lint, release, table, document_path);
+  });
+}
+
+function validateDocument(
+  lint: boolean,
+  release: boolean,
+  table: object,
+  document_path: string,
+): void {
+  const document: string = readFileSync(document_path);
+  // ...
 }
