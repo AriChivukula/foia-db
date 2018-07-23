9f40fb1be9754585e0d102835418a1c4d49d2d49
Ari Chivukula <ari@chivuku.la>
2018-07-23 14:37:20 -0700

    Update index.ts

--- source/index.ts
+++ source/index.ts
@@ -1,4 +1,4 @@
 import "@babel/polyfill";
 import { readFileSync } from "fs";
 
-export const Client: any = JSON.parse(readFileSync(".foia-db.json", "ascii"));;
+export const DB: any = JSON.parse(readFileSync(".foia-db.json", "ascii"));;
