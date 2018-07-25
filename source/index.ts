b0d4c1363c103a1299a20028a99e6149a147a581
Ari Chivukula <ari@chivuku.la>
2018-07-25 14:39:59 -0700

    Update index.ts

--- source/index.ts
+++ source/index.ts
@@ -1,16 +1,38 @@
 import "@babel/polyfill";
-import { readFileSync } from "fs";
-// @ts-ignore
-import * as gremlin from "gremlin";
+import { readFileSync, writeFileSync } from "fs";
 
-export class FOIAGraphSource extends gremlin.process.GraphTraversalSource {
-}
+export class Graph {
 
-export class FOIAGraph extends gremlin.structure.Graph {
-  
-  public traversal(): FOIAGraphSource {
-    return new FOIAGraphSource(this, new gremlin.process.TraversalStrategies());
+  public static new(): Graph {
+    return new Graph();
+  }
+
+  public static read(): Graph {
+    return new Graph();
+  }
+
+  private constructor() {}
+
+  public write(): void {
+  }
+
+  public property(name: string, value: any): Graph {
+    return this;
+  }
+
+  public addVertex(label: string): Graph {
+    return this;
+  }
+
+  public V(): Graph {
+    return this;
+  }
+
+  public hasLabel(label: string): Graph {
+    return this;
+  }
+
+  public count(): number {
+    return this;
   }
 }
-
-export const DB: gremlin.process.GraphTraversalSource = (new FOIAGraph()).traversal();
