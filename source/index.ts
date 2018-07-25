59cb4774683dd2ca5d6e7cbe2016d471e2ce1e21
Ari Chivukula <ari@chivuku.la>
2018-07-25 11:20:45 -0700

    Update index.ts

--- source/index.ts
+++ source/index.ts
@@ -1,5 +1,6 @@
 import "@babel/polyfill";
+import { readFileSync } from "fs";
 // @ts-ignore
 import * as gremlin from "gremlin";
 
-export const DB: gremlin.Graph = (new gremlin.structure.io.GraphSONReader()).read(".foia-db.json");
+export const DB: gremlin.Graph = (new gremlin.structure.io.GraphSONReader()).read(readFileSync(".foia-db.json", "ascii"));
