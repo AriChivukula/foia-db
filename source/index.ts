b0d472e662b70ec5ae2c45a8108d18cae7aae0b2
Ari Chivukula <ari@chivuku.la>
2018-07-25 16:24:34 -0700

    Update index.ts

--- source/index.ts
+++ source/index.ts
@@ -1,19 +1,29 @@
 import "@babel/polyfill";
 import { readFileSync, writeFileSync } from "fs";
 
+interface EdgeStorage {
+  label: string;
+  source_id: any;
+  source_label: string;
+  target_id: any;
+  target_label: string;
+}
+
 interface VertexStorage {
  label: string;
  properties: any;
 }
 
 interface GraphStorage {
- vertices: VertexStorage[];
+  edges: EdgeStorage[];
+  vertices: VertexStorage[];
 }
 
 export class Graph {
-  
+
   private storage: GraphStorage;
   private vertexToWrite?: VertexStorage;
+  private edgeToWrite?: EdgeStorage;
   private verticesToRead?: VertexStorage[];
 
   public static new(): Graph {
@@ -29,6 +39,7 @@ export class Graph {
       this.storage = JSON.parse(readFileSync(".foia-db.json", "ascii"));
     } else {
       this.storage = {
+        edges: [],
         vertices: [],
       };
     }
@@ -36,16 +47,17 @@ export class Graph {
 
   private endWrites(): void {
     this.vertexToWrite = undefined;
+    this.edgeToWrite = undefined;
   }
 
   private endReads(): void {
     this.verticesToRead = undefined;
   }
- 
+
   public write(): void {
     writeFileSync(".foia-db.json", JSON.stringify(this.storage))
   }
- 
+
   /* Write */
 
   public addV(label: string): Graph {
@@ -57,12 +69,25 @@ export class Graph {
     this.storage.vertices = this.storage.vertices.concat([this.vertexToWrite]);
     return this;
   }
- 
+
+  public addE(label: string, vertex: VertexStorage): Graph {
+    this.endReads();
+    this.edgeToWrite = {
+      label,
+      source_label = (this.vertexToWrite as VertexStorage).label,
+      source_id: (this.vertexToWrite as VertexStorage).properties["id"],
+      target_id: vertex.properties["id"],
+      target_label = vertex.label,
+    };
+    this.storage.edges = this.storage.edges.concat([this.edgeToWrite]);
+    return this;
+  }
+
   public property(key: string, value: any): Graph {
     (this.vertexToWrite as VertexStorage).properties[key] = value;
     return this;
   }
- 
+
   /* Read */
 
   public V(): Graph {
@@ -75,7 +100,11 @@ export class Graph {
     this.verticesToRead = (this.verticesToRead as VertexStorage[]).filter(vertex => vertex.label === label);
     return this;
   }
- 
+
+  public outE(label: string): Graph {
+    return this;
+  }
+
   /* Terminal */
 
   public count(): number {
