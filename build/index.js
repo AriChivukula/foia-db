d6b45f95d9b639f465e95ce3cabd2128b29e7722
Ari Chivukula <ari@chivuku.la>
2018-07-25 12:04:12 -0700

    Delete index.js

--- build/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-require("@babel/polyfill");
-var fs_1 = require("fs");
-var gremlin = require("gremlin");
-exports.DB = (new gremlin.structure.io.GraphSONReader()).read(JSON.parse(fs_1.readFileSync(".foia-db.json", "ascii")));
