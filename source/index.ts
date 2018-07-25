5410e62631eaefa6a798bdd3a55a6ad11b09ac93
Ari Chivukula <ari@chivuku.la>
2018-07-25 12:41:56 -0700

    Update index.ts

--- source/index.ts
+++ source/index.ts
@@ -6,8 +6,5 @@ import * as gremlin from "gremlin";
 export const DB: gremlin.process.GraphTraversal = new gremlin.process.GraphTraversalSource(
   new gremlin.structure.Graph(),
   new gremlin.process.TraversalStrategies(),
-  {
-    sourceInstructions: [],
-    stepInstructions: (new gremlin.structure.io.GraphSONReader()).read(JSON.parse(readFileSync(".foia-db.json", "ascii")))["steps"],
-  },
+  JSON.parse(readFileSync(".foia-db.json", "ascii")),
 );
