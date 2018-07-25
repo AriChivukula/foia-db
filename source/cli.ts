c4e673e7bb87ef1e618627b53edddfa89f7b07f9
Ari Chivukula <ari@chivuku.la>
2018-07-25 10:00:46 -0700

    Update cli.ts

--- source/cli.ts
+++ source/cli.ts
@@ -37,7 +37,7 @@ function validateConfig(
   });
   if (compile) {
     console.log("Writing DB");
-    writeFileSync(".foia-db.json", gremlin.structure.io.GraphSONWriter.write(graph));
+    writeFileSync(".foia-db.json", (new gremlin.structure.io.GraphSONWriter()).write(graph));
   }
 }
 
