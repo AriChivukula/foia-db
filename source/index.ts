916c39136fba0734d8973d7af83ca264d09342d6
Ari Chivukula <ari@chivuku.la>
2018-07-25 11:37:38 -0700

    Update index.ts

--- source/index.ts
+++ source/index.ts
@@ -3,4 +3,4 @@ import { readFileSync } from "fs";
 // @ts-ignore
 import * as gremlin from "gremlin";
 
-export const DB: gremlin.Graph = (new gremlin.structure.io.GraphSONReader()).read(readFileSync(".foia-db.json", "ascii"));
+export const DB: gremlin.Graph = (new gremlin.structure.io.GraphSONReader()).read(JSON.parse(readFileSync(".foia-db.json", "ascii")));
