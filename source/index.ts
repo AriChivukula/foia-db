49e2a0ba0ee8684258ab26bb2be2aa65553cecfa
Ari Chivukula <ari@chivuku.la>
2018-07-25 13:39:35 -0700

    Update index.ts

--- source/index.ts
+++ source/index.ts
@@ -3,11 +3,7 @@ import { readFileSync } from "fs";
 // @ts-ignore
 import * as gremlin from "gremlin";
 
-export const DB: gremlin.process.GraphTraversal = new gremlin.process.GraphTraversalSource(
-  new gremlin.structure.Graph(),
-  new gremlin.process.TraversalStrategies(),
-  {
-    sourceInstructions: [],
-    stepInstructions: JSON.parse(readFileSync(".foia-db.json", "ascii")),
-  },
-);
+export class FOIAGraph extends gremlin.structure.Graph {
+}
+
+export const DB: gremlin.process.GraphTraversal = (new FOIAGraph()).traversal();
