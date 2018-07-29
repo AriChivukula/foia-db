42353532021920bf091a5bb14e1dd5f57dc0e9e8
Ari Chivukula <ari@chivuku.la>
2018-07-29 09:07:35 -0700

    Update read.ts

--- source/read.ts
+++ source/read.ts
@@ -43,7 +43,7 @@ export function mi(id: any): MetadataID {
 export interface IProperty {
   "id": PropertyID;
   "metadata": {[idx: string]: {
-    "id": MetadataID;
+    "id": MetadataID,
   };
 }
 
