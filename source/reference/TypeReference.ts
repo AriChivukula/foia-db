abc71b34803ebe96de651cb6da147a69cb0a9ffb
Ari Chivukula <ari@chivuku.la>
2018-08-20 09:40:56 -0700

    Update TypeReference.ts

--- source/reference/TypeReference.ts
+++ source/reference/TypeReference.ts
@@ -1,1 +1,59 @@
-export {};
+export interface ITypeReference {
+  readonly typeName: string;
+  readonly isList: boolean;
+}
+
+export class TypeReference {
+  
+  public static boolean(): TypeReference {
+    return new TypeReference({
+      typeName: "boolean",
+      isList: false,
+    });
+  }
+  
+  public static booleanArray(): TypeReference {
+    return new TypeReference({
+      name: "boolean",
+      isList: true,
+    });
+  }
+
+  public static number(): TypeReference {
+    return new TypeReference({
+      typeName: "number",
+      isList: false,
+    });
+  }
+  
+  public static numberArray(): TypeReference {
+    return new TypeReference({
+      name: "number",
+      isList: true,
+    });
+  }
+  
+  public static string(): TypeReference {
+    return new TypeReference({
+      typeName: "string",
+      isList: false,
+    });
+  }
+  
+  public static stringArray(): TypeReference {
+    return new TypeReference({
+      name: "string",
+      isList: true,
+    });
+  }
+
+  private constructor(
+    private readonly props: ITypeReference,
+  ) {
+    super();
+  }
+  
+  public name(): string {
+    return this.props.typeName + (this.props.isList ? "[]" : "");
+  }
+}
