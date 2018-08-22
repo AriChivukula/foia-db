d43d283496941f82ab7ae4da0ded919895599480
Ari Chivukula <ari@chivuku.la>
2018-08-22 10:05:27 -0700

    Update references.ts

--- test/references.ts
+++ test/references.ts
@@ -4,6 +4,7 @@ import "mocha";
 import * as chai from "chai";
 
 import {
+  KindReference,
   TypeReference,
 } from "../source/index";
 
@@ -13,5 +14,13 @@ it(
     chai.expect(TypeReference.string().name()).to.equal("string");
     chai.expect(TypeReference.numberArray().name()).to.equal("number[]");
     chai.expect(TypeReference.booleanArray().name()).to.not.equal(TypeReference.boolean().name());
+  },
+);
+
+it(
+  "KindReference",
+  async (): Promise<void> => {
+    chai.expect(KindReference.get("test").name()).to.equal("test");
+    chai.expect(KindReference.get("test").name()).to.not.equal(KindReference.get("test").name());
   },
 );
