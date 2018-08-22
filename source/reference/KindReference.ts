a13f638283f558fcf6cff71b3826d963f5642c0f
Ari Chivukula <ari@chivuku.la>
2018-08-22 10:03:28 -0700

    Update KindReference.ts

--- source/reference/KindReference.ts
+++ source/reference/KindReference.ts
@@ -1,1 +1,21 @@
-export {};
+export interface IKindReference {
+  readonly kindName: string;
+}
+
+export class KindReference {
+
+  public static get(name: string): KindReference {
+    return new KindReference({
+      kindName: name,
+    });
+  }
+
+  private constructor(
+    private readonly props: IKindReference,
+  ) {
+  }
+
+  public name(): string {
+    return this.props.kindName;
+  }
+}
