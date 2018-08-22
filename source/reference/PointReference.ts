63d3acf4ba0c665f9c6e6d45ba0b4cf9e7b2eb0b
Ari Chivukula <ari@chivuku.la>
2018-08-22 16:59:41 -0700

    Update PointReference.ts

--- source/reference/PointReference.ts
+++ source/reference/PointReference.ts
@@ -1,1 +1,31 @@
-export {};
+import {
+  KindReference,
+} from "./KindReference";
+
+export interface IPointReference {
+  readonly kindReference: KindReference;
+  readonly pointName: string;
+}
+
+export class PointReference {
+
+  public static get(pointName: string, kindReference: KindReference): PointReference {
+    return new PointReference({
+      pointName,
+      kindReference,
+    });
+  }
+
+  private constructor(
+    private readonly props: IPointReference,
+  ) {
+  }
+
+  public name(): string {
+    return this.props.pointName;
+  }
+  
+  public kind(): KindReference {
+    return this.props.kindReference;
+  }
+}
