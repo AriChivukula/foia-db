7c5f9b04e8df5c6ba39016450a9274fe8360cdaf
Ari Chivukula <ari@chivuku.la>
2018-08-25 20:01:37 -0700

    Update LineReference.ts

--- source/reference/LineReference.ts
+++ source/reference/LineReference.ts
@@ -1,1 +1,31 @@
-export {};
+import {
+  PointReference,
+} from "./PointReference";
+
+export interface ILineReference {
+  readonly fromPointReference: PointReference;
+  readonly toPointReference: PointReference;
+}
+
+export class LineReference {
+
+  public static get(fromPointReference: PointReference, toPointReference: PointReference): LineReference {
+    return new LineReference({
+      fromPointReference,
+      toPointReference,
+    });
+  }
+
+  private constructor(
+    private readonly props: ILineReference,
+  ) {
+  }
+
+  public fromPoint(): PointReference {
+    return this.props.fromPointReference;
+  }
+  
+  public toPoint(): PointReference {
+    return this.props.toPointReference;
+  }
+}
