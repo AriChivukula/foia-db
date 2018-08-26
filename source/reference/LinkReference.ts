26d0eeb55a1780a95408a6569b991849d688dd63
Ari Chivukula <ari@chivuku.la>
2018-08-25 19:58:04 -0700

    Update LinkReference.ts

--- source/reference/LinkReference.ts
+++ source/reference/LinkReference.ts
@@ -1,1 +1,31 @@
-export {};
+import {
+  KindReference,
+} from "./KindReference";
+
+export interface ILinkReference {
+  readonly fromKindReference: KindReference;
+  readonly toKindReference: KindReference;
+}
+
+export class LinkReference {
+
+  public static get(fromKindReference: KindReference, toKindReference: KindReference): LinkReference {
+    return new LinkReference({
+      fromKindReference,
+      toKindReference,
+    });
+  }
+
+  private constructor(
+    private readonly props: ILinkReference,
+  ) {
+  }
+
+  public toKind(): KindReference {
+    return this.props.fromKindReference;
+  }
+  
+  public fromKind(): KindReference {
+    return this.props.toKindReference;
+  }
+}
