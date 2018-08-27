a29187b987e3ee89d5438e6d8827a6caa1a171f3
Ari Chivukula <ari@chivuku.la>
2018-08-26 17:39:05 -0700

    Update MetapropertyReference.ts

--- source/reference/MetapropertyReference.ts
+++ source/reference/MetapropertyReference.ts
@@ -6,17 +6,17 @@ import {
 } from "./TypeReference";
 
 export interface IMetapropertyReference {
-  readonly propertyReference: PropertyReference;
   readonly name: string;
+  readonly propertyReference: PropertyReference;
   readonly typeReference: TypeReference;
 }
 
 export class MetapropertyReference {
 
-  public static get(propertyReference: PropertyReference, name: string, typeReference: TypeReference): MetapropertyReference {
+  public static get(name: string, propertyReference: PropertyReference, typeReference: TypeReference): MetapropertyReference {
     return new MetapropertyReference({
-      propertyReference,
       name,
+      propertyReference,
       typeReference,
     });
   }
@@ -26,14 +26,14 @@ export class MetapropertyReference {
   ) {
   }
 
-  public property(): PropertyReference {
-    return this.props.propertyReference;
-  }
-  
   public name(): string {
     return this.props.name;
   }
-  
+
+  public property(): PropertyReference {
+    return this.props.propertyReference;
+  }
+
   public type(): TypeReference {
     return this.props.typeReference;
   }
