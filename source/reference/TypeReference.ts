15a99f11a3636604d8a8f3b80675abed25bdfac4
Ari Chivukula <ari@chivuku.la>
2018-08-26 17:42:03 -0700

    Update TypeReference.ts

--- source/reference/TypeReference.ts
+++ source/reference/TypeReference.ts
@@ -1,49 +1,49 @@
 export interface ITypeReference {
-  readonly typeName: string;
-  readonly isList: boolean;
+  readonly list: boolean;
+  readonly name: string;
 }
 
 export class TypeReference {
   
   public static boolean(): TypeReference {
     return new TypeReference({
-      typeName: "boolean",
-      isList: false,
+      list: false,
+      name: "boolean",
     });
   }
   
   public static booleanArray(): TypeReference {
     return new TypeReference({
-      typeName: "boolean",
-      isList: true,
+      list: true,
+      name: "boolean",
     });
   }
 
   public static number(): TypeReference {
     return new TypeReference({
-      typeName: "number",
-      isList: false,
+      list: false,
+      name: "number",
     });
   }
   
   public static numberArray(): TypeReference {
     return new TypeReference({
-      typeName: "number",
-      isList: true,
+      list: true,
+      name: "number",
     });
   }
   
   public static string(): TypeReference {
     return new TypeReference({
-      typeName: "string",
-      isList: false,
+      list: false,
+      name: "string",
     });
   }
   
   public static stringArray(): TypeReference {
     return new TypeReference({
-      typeName: "string",
-      isList: true,
+      list: true,
+      name: "string",
     });
   }
 
