7f6fb7d2bcf24d4340c3ec9abe44ca49e57c8756
Ari Chivukula <ari@chivuku.la>
2019-04-12 16:18:54 -0700

    Update index.ts

--- source/index.ts
+++ source/index.ts
@@ -6,14 +6,15 @@ import {
 } from 'fs';
 import {
   DBSchema,
+  IDBPDatabase,
   openDB,
 } from 'idb';
 
 export async function readIndexedDB<T extends DBSchema>(directory: string): Promise<IDBPDatabase<T>> {
   const mod: { default: T } = require(`${directory}/schema`);
-  const data = await promises.readFile(`${directory}/schema.json`);
+  const data = await promises.readFile(`${directory}/schema.json`, { encoding: true });
   const jsonData = JSON.parse(data);
-  return await openDB<T>(
+  const db = await openDB<T>(
     "db",
     0,
     {
@@ -25,4 +26,5 @@ export async function readIndexedDB<T extends DBSchema>(directory: string): Prom
       }
     },
   );
+  return db;
 }
